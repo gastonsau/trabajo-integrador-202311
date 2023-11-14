7f22cb91b578762adeb558618798e9c2681693d9
gastonsau <126187288+gastonsau@users.noreply.github.com>
2023-11-14 15:33:17 -0300

    modificacion

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -16,7 +16,7 @@ const TaskList = () => {
   const listTaskStorageParsed = JSON.parse(listTaskStorage)
 
   // Verifica si el valor del array obtenido en el LocalStorage no está vacio y lo asigna al valor inicial del array que contiene la lista de tareas
-  if(listTaskStorageParsed.length() >1)
+  if(listTaskStorageParsed.length >1)
   listTask = listTaskStorageParsed
 
   // Setea el estado inicial con el array
