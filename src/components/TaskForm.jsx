68467892e7661b1d290c1f93b309c9a7d3b86e3b
plmperez <82291364+pauloperezdev@users.noreply.github.com>
2023-11-17 11:22:37 +0000

    Agregada la funcion boton mas para agregar la tarea al hacer click

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-const TaskForm = ({addNewTask}) => {
-  
-    const  [inputItem, setInputItem] = useState('')
-    const onInputChange = (event) => {
-        setInputItem(event.target.value)
-    }
-    const onSubmit = (event) => {
+const TaskForm = ({ addNewTask }) => {
+  const [inputItem, setInputItem] = useState("");
+  const onInputChange = (event) => {
+    setInputItem(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    addNewTask(inputItem);
+    setInputItem("");
+  };
 
-        event.preventDefault()
-        addNewTask(inputItem)
-        setInputItem('')
-    }
-
+  const botonMas = () => {
+    addNewTask(inputItem);
+    setInputItem("");    
+  }
 
   return (
-    <form className='add' onSubmit={onSubmit}>
-        <input 
-          type = 'text' 
-          name = 'add' 
-          placeholder = 'Ingrese la nueva tarea' 
-          value = {inputItem} 
-          onChange = {onInputChange} 
-        />
-        <div className='input-buttons'>
-          <i className='fas fa-plus add'></i>
-        </div>
+    <form className="add" onSubmit={onSubmit}>
+      <input
+        type="text"
+        name="add"
+        placeholder="Ingrese la nueva tarea"
+        value={inputItem}
+        onChange={onInputChange}
+      />
+      <div className="input-buttons">
+        <i className="fas fa-plus add" onClick={botonMas}></i>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default TaskForm
+export default TaskForm;
